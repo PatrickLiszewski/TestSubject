e7f11230f9df0dd3545fdd5227c3d969f36a92f7
PatrickLiszewski <pj03lisz@siena.edu>
2025-05-01 16:34:12 -0400

    Updated code so that reloading pages or joining late does not desync the shape. Also updated methods and emit messages to be more streamlined as some overlapped

--- server.js
+++ server.js
@@ -8,27 +8,29 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Properties of the Test Subject are saved here as global variables
+let currentColor = 'red';
+let currentSize = '100px';
+
 // Serve static files from "public" folder
 app.use(express.static('public'));
 
 // Socket.IO connection handler
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
+  socket.emit('updateColor', currentColor);
+  socket.emit('changeSize', currentSize);
 
   socket.on('changeColor', (color) => {
     console.log('Color changed to:', color);
-    // Broadcast to all clients (including sender)
+    currentColor = color;
     io.emit('updateColor', color);
   });
 
-  socket.on('bigger', (size) => {
-    console.log('time to get bigger!');
-    io.emit('makeBig', size);
-  });
-
-  socket.on('smaller', (size) => {
-    console.log('time to get smaller!');
-    io.emit('makeSmall', size);
+  socket.on('sendSize', (size) => {
+    console.log('time to change size!');
+    currentSize = size;
+    io.emit('changeSize', size);
   });
 
   socket.on('disconnect', () => {
